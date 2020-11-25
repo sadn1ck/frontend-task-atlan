3f9aed78f7fd98f5e0dd18c0ecde25a463e3fb24
Anik Das <anikdas0811@gmail.com>
2020-11-25 11:26:57 +0530

    - upd: added hero, forgot to add in last
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

---
+++ components/landing/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="body-font">
+    <div
+      class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+    >
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <div
+          class="mx-auto text-center rounded"
+          style="background-color: #ffffff; height: 300px; width: 360px"
+        />
+      </div>
+      <div
+        class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"
+      >
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">
+          Your IPL Database
+        </h1>
+        <p class="mb-8 leading-relaxed">
+          Leverage agile frameworks to provide a robust synopsis for high level
+          overviews. Iterative approaches to corporate strategy foster
+          collaborative thinking to further the overall value proposition.
+          Organically grow disruptive innovation via workplace diversity and
+          empowerment.
+        </p>
+        <div
+          class="flex w-full md:justify-start justify-center items-end space-x-5"
+        >
+          <div class="inline-flex border-0 btn">Search</div>
+          <button class="inline-flex border-0 btn">Subscribe</button>
+        </div>
+        <p class="text-sm mt-2 mb-8 w-full">
+          Never fear, this doesn't actually do anything! (Or does it?)
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
